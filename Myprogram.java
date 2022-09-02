56004e22db19895d820457dcc494cd7b8ea2084f
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-09-02 13:56:44 +0530

    Add files via upload

---
+++ Myprogram.java
@@ -0,0 +1,20 @@
+// fix the compilation error and invoke the method printmessage of display message class.
+package testprogram;
+
+
+class displayMymessage{
+	void printMessage() {
+		System.out.println("Hello My Message");
+	}
+
+}
+public class Myprogram {
+
+	public static void main(String[] args) {
+		displayMymessage ag=new displayMymessage();
+		
+		ag.printMessage();
+	
+
+}
+}
