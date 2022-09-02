9f4c012c0860207ad8ac4d2b64710b9511cebed5
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-09-02 13:56:11 +0530

    Add files via upload

---
+++ classrectangle.java
@@ -0,0 +1,19 @@
+package Com.congnizet.shapes;
+
+
+class AreaCalculator{
+	void calculateArea() {
+		System.out.println("the area of rectangle is length*breadth");
+	}
+}
+
+public class classrectangle {
+
+	public static void main(String[] args) {
+		AreaCalculator ac=new AreaCalculator();
+		
+		ac.calculateArea();
+
+	}
+
+}
