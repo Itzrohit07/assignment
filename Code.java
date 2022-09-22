b234cdcbfbe44f5ce03aed6e635527509bbf4a01
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-09-22 14:11:29 +0530

    Add files via upload

---
+++ Code.java
@@ -0,0 +1,14 @@
+package testprogram;
+
+public class Code {
+	public static void main(String[]args) {
+		System.out.println("Devided by 5:");
+		for (int i=1;i<200;i++) {
+			if(i%5==0 || i%7==0) {
+				System.out.println(i);
+		}
+	}
+
+}
+
+}
